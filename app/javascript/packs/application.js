ec1333a4036b072c48ef2c21f7b55d6ca0ffef0e
Chengjie Shen <362700182@qq.com>
2022-06-16 19:29:30 +0800

    Great

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,16 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// Bootstrap import
+import "bootstrap";
+
+// Stimulus import
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+
+// Stimulus
+const application = Application.start()
+const context = require.context("controllers", true, /_controller\.js$/)
+application.load(definitionsFromContext(context))
